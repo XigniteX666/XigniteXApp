9fba08b6b91ed11b411814075d82dbba1405c944
Oskar Albers <o.albers@blokkerholding.nl>
2018-03-16 14:50:24 +0100

    aa

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var session = require('express-session');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -10,9 +12,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.use(session({
+  secret: 'sshhggss'
+}));
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +29,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
+
+app.get('/api/movies', function(req, res){
+  console.log("Movies")
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +54,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+app.listen(8080)
+console.log("App started at port 8080")
 
 module.exports = app;
